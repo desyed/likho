201ace05afd34b9d9c5a1506433f9a1cf8be9f64
desyed <syedshihabdu@gmail.com>
2023-08-04 17:15:47 +0600

    getting started

---
+++ src/app/(dashboard)/playground/[project]/[page]/page.tsx
@@ -0,0 +1,28 @@
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
+
+        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
+          <a
+            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
+            href="/"
+          >
+            <Image
+              src="/likho.svg"
+              alt="likho Logo"
+              className="dark:invert"
+              width={100}
+              height={24}
+              priority
+            />
+          </a>
+        </div>
+      </div>
+
+      <div> this is project {'>'} page editor: should contain a sidebar, topbar and body editor</div>
+    </main>
+  )
+}
