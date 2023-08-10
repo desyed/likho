b3e00616da89665d53e9b496459724dd4c570361
desyed <syedshihabdu@gmail.com>
2023-08-10 22:42:58 +0600

    had a mistake

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,7 +1,7 @@
 // middleware.ts
 import { NextResponse } from 'next/server';
 import type { NextRequest } from 'next/server';
-import { getValidSubdomain } from '@/utils/subdomain';
+// import { getValidSubdomain } from '@/utils/subdomain';
 import {isSubdomain} from "@/lib/utils";
 
 // RegExp for public files
