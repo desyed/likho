0466a77960968b8f9458aec5c153506237f241af
desyed <syedshihabdu@gmail.com>
2023-08-12 23:18:37 +0600

    middleware updated

--- next.config.js
+++ next.config.js
@@ -4,12 +4,18 @@ const nextConfig = {
         domains: ['localhost', 'res.cloudinary.com', 'lh3.googleusercontent.com'],
     },
     staticPageGenerationTimeout: 100,
-    async rewrites() {
+    async headers() {
         return [
             {
-                source: '/api/:path*',
-                destination: 'https://www.likho.site/:path*',
-            },
+                // matching all API routes
+                source: "/api/:path*",
+                headers: [
+                    { key: "Access-Control-Allow-Credentials", value: "true" },
+                    { key: "Access-Control-Allow-Origin", value: "*" },
+                    { key: "Access-Control-Allow-Methods", value: "GET,OPTIONS,PATCH,DELETE,POST,PUT" },
+                    { key: "Access-Control-Allow-Headers", value: "Authorization, x-api-key, X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
+                ]
+            }
         ]
     },
 }
