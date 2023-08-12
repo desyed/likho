18fc06617c5ae01af368bd132dccb1ba888bfc5b
desyed <syedshihabdu@gmail.com>
2023-08-12 21:10:23 +0600

    middleware updated

--- src/middleware.ts
+++ src/middleware.ts
@@ -6,6 +6,20 @@ import {getValidSubdomain} from "@/lib/utils";
 const PUBLIC_FILE = /\.(.*)$/; // Files
 
 export async function middleware(req: NextRequest) {
+
+  // retrieve the current response
+  const res = NextResponse.next()
+
+  // add the CORS headers to the response
+  res.headers.append('Access-Control-Allow-Credentials', "true")
+  res.headers.append('Access-Control-Allow-Origin', '*') // replace this your actual origin
+  res.headers.append('Access-Control-Allow-Methods', 'GET,DELETE,PATCH,POST,PUT')
+  res.headers.append(
+      'Access-Control-Allow-Headers',
+      'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version, Authorization, x-api-key',
+  )
+
+
   // Clone the URL
   const url = req.nextUrl.clone();
 
