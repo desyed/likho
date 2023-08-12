d0da575a7b7542ed60cc499807d0db7ed526464b
desyed <syedshihabdu@gmail.com>
2023-08-12 23:25:07 +0600

    middleware updated

--- next.config.js
+++ next.config.js
@@ -4,20 +4,7 @@ const nextConfig = {
         domains: ['localhost', 'res.cloudinary.com', 'lh3.googleusercontent.com'],
     },
     staticPageGenerationTimeout: 100,
-    async headers() {
-        return [
-            {
-                // matching all API routes
-                source: "/api/:path*",
-                headers: [
-                    { key: "Access-Control-Allow-Credentials", value: "true" },
-                    { key: "Access-Control-Allow-Origin", value: "*" },
-                    { key: "Access-Control-Allow-Methods", value: "GET,OPTIONS,PATCH,DELETE,POST,PUT" },
-                    { key: "Access-Control-Allow-Headers", value: "Authorization, x-api-key, X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
-                ]
-            }
-        ]
-    },
+
 }
 
 module.exports = nextConfig
