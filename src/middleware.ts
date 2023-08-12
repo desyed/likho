0fc98240ddc9f8056d122c5bae31008cc530444a
desyed <syedshihabdu@gmail.com>
2023-08-12 23:09:14 +0600

    middleware updated

--- src/middleware.ts
+++ src/middleware.ts
@@ -7,16 +7,7 @@ const PUBLIC_FILE = /\.(.*)$/; // Files
 
 export async function middleware(req: NextRequest) {
 
-  // retrieve the current response
-  const res = NextResponse.next()
 
-  // add the CORS headers to the response
-  res.headers.append('Access-Control-Allow-Credentials', "true")
-  res.headers.append('Access-Control-Allow-Origin', 'http://localhost:3000, https://www.likho.site, https://likho.site')
-  res.headers.append('Access-Control-Allow-Methods', 'GET,DELETE,PATCH,POST,PUT')
-  res.headers.append('Access-Control-Allow-Headers',
-      'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version, Authorization, x-api-key',
-  )
 
 
   // Clone the URL
